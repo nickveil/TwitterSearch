e24468e45932699af72d32e1baa0d23077a273fb
nickveil <31191433+nickveil@users.noreply.github.com>
2017-12-22 18:53:42 -0500

    added pics and set up file to respond to incoming tweets, not tested yet

--- app/search.js
+++ app/search.js
@@ -9,46 +9,63 @@ var fs = require('fs');
 
 
 
-// // ***** Sets up user stream ********
-// var stream = T.stream('user');
+// ***** Sets up user stream ********
+var stream = T.stream('user');
 
 
-// // ***** Listening for any followers ******
-// stream.on('follow', followed);
+// ***** Listening for any followers ******
+stream.on('tweet', tweetEvent);
 
-// function followed (event){
-// 	console.log("Follow event");
-// 	var name = event.source.name;
-// 	var screenName = event.source.screen_name;
-// 	tweetIt('@' + screenName + ' Thanks for following me!!!');
-// 	console.log(event);
-// };
-tweetIt();
+function tweetEvent (event){
+
+	var replyto = event.in_reply_to_screen_name;
+	var text = event.text;
+	var from = event.user.screen_name;
 
 
-function tweetIt() {
-	var cmd = 'processing-java --sketch=`pwd`/welcome --run';
-	exec(cmd, processing);
+console.log(replyto +' ' + from);
+	if (replyto ==='NvTwitp'){
+		var newTweet = '@' + from + ' Thanks for the tweet here is a picture of one of my dogs.'
+	}
+	
 
-	function processing(){
-		var filename = 'welcome/output.png';
-		var params = {
-			encoding: 'base64'
+	// var json = JSON.stringify(event,null,2);
+	// fs.writeFile("tweet.json",json);
+
+
+	// console.log("Follow event");
+	// var name = event.source.name;
+	// var screenName = event.source.screen_name;
+	// tweetIt('@' + screenName + ' Thanks for following me!!!');
+	// console.log(event);
+};
+// tweetIt();
+
+
+function tweetIt(txt) {
+	// var cmd = 'processing-java --sketch=`pwd`/welcome --run';
+	// exec(cmd, processing);
+
+	// function processing(){
+	var randNum = Math.floor(Math.random()*100);
+	var filename = 'pic/dog'+randNum +'.png';
+	var params = {
+		encoding: 'base64'
+	}
+	var b64 = fs.readFileSync(filename, params)
+
+	T.post('media/upload', {media_data: b64 }, uploaded);
+
+	function uploaded (err, data, response){
+		var id = data.media_id_string;
+		var tweet = {
+			status: txt,
+			media_ids: [id]
 		}
-		var b64 = fs.readFileSync(filename, params)
+		T.post('statuses/update',tweet, tweeted);
+	}
 
-		T.post('media/upload', {media_data: b64 }, uploaded);
-
-		function uploaded (err, data, response){
-			var id = data.media_id_string;
-			var tweet = {
-				status: '#HelloWorld from Node.js.',
-				media_ids: [id]
-			}
-			T.post('statuses/update',tweet, tweeted);
-		}
-
-		console.log('finished');
+	console.log('finished');
 	}
 
 	// var randNum = Math.floor(Math.random()*100);
@@ -59,7 +76,7 @@ function tweetIt() {
 		if (err){
 			console.log("Nope it didn't work ");
 		} else {
-			console.log("I think it worked, go check your profile! ")
+			console.log("I think it worked, go check your profile! ");
 		}
 	}
 
